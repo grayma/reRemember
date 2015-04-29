8ba641887f2fe137968b840092715c0716769f70
Matthew Gray <matt@mattgray.net>
2015-04-29 12:12:00 -0400

    Results and study view finalized along with TODO.

--- reRemember/StudyView.Designer.cs
+++ reRemember/StudyView.Designer.cs
@@ -28,64 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            System.Windows.Forms.Label labelCard;
+            this.buttonFlip = new System.Windows.Forms.Button();
+            this.buttonCorrect = new System.Windows.Forms.Button();
+            this.buttonIncorrect = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.buttonFinish = new System.Windows.Forms.Button();
+            labelCard = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonFlip
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.buttonFlip.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(12, 277);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(410, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Flip Card";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonFlip.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFlip.Location = new System.Drawing.Point(12, 252);
+            this.buttonFlip.Name = "buttonFlip";
+            this.buttonFlip.Size = new System.Drawing.Size(410, 23);
+            this.buttonFlip.TabIndex = 0;
+            this.buttonFlip.Text = "&Flip Card";
+            this.buttonFlip.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // buttonCorrect
             // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(12, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Correct";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonCorrect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCorrect.Location = new System.Drawing.Point(12, 12);
+            this.buttonCorrect.Name = "buttonCorrect";
+            this.buttonCorrect.Size = new System.Drawing.Size(92, 23);
+            this.buttonCorrect.TabIndex = 1;
+            this.buttonCorrect.Text = "&Correct";
+            this.buttonCorrect.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // buttonIncorrect
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(330, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(92, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Incorrect";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonIncorrect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonIncorrect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonIncorrect.Location = new System.Drawing.Point(330, 12);
+            this.buttonIncorrect.Name = "buttonIncorrect";
+            this.buttonIncorrect.Size = new System.Drawing.Size(92, 23);
+            this.buttonIncorrect.TabIndex = 2;
+            this.buttonIncorrect.Text = "&Incorrect";
+            this.buttonIncorrect.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // labelStatus
             // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label1.Location = new System.Drawing.Point(110, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(214, 23);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Card 15/30";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelStatus.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelStatus.Location = new System.Drawing.Point(110, 12);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(214, 23);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCard
+            // 
+            labelCard.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            labelCard.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            labelCard.Location = new System.Drawing.Point(9, 38);
+            labelCard.Name = "labelCard";
+            labelCard.Size = new System.Drawing.Size(413, 211);
+            labelCard.TabIndex = 4;
+            labelCard.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNext.Location = new System.Drawing.Point(249, 281);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(173, 23);
+            this.buttonNext.TabIndex = 6;
+            this.buttonNext.Text = "> &Next Card >";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonPrevious.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPrevious.Location = new System.Drawing.Point(12, 281);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(173, 23);
+            this.buttonPrevious.TabIndex = 7;
+            this.buttonPrevious.Text = "< &Previous Card <";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            // 
+            // buttonFinish
+            // 
+            this.buttonFinish.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonFinish.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFinish.Location = new System.Drawing.Point(191, 281);
+            this.buttonFinish.Name = "buttonFinish";
+            this.buttonFinish.Size = new System.Drawing.Size(52, 23);
+            this.buttonFinish.TabIndex = 8;
+            this.buttonFinish.Text = "Finish";
+            this.buttonFinish.UseVisualStyleBackColor = true;
             // 
             // StudyView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(434, 312);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonFinish);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(labelCard);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonIncorrect);
+            this.Controls.Add(this.buttonCorrect);
+            this.Controls.Add(this.buttonFlip);
             this.MinimumSize = new System.Drawing.Size(450, 350);
             this.Name = "StudyView";
             this.Text = "reRemember - Studying";
@@ -95,9 +149,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonFlip;
+        private System.Windows.Forms.Button buttonCorrect;
+        private System.Windows.Forms.Button buttonIncorrect;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Button buttonFinish;
     }
 }
